8850f723c3357dcbdb9932d5f1d8475a292b44e9
ArtemSharapanyk <artmencd2003@gmail.com>
2021-04-27 14:55:30 +0300

    fix something

--- src/js/skillsTabs.js
+++ src/js/skillsTabs.js
@@ -4,49 +4,68 @@ export default (els, tabs) => {
     let clickedEls = document.querySelectorAll(els);
     let tab        = document.querySelectorAll(tabs);
 
+    console.log(clickedEls, tab)
+
     let heigthOfTab = '20%';
 
 
-    let activeEls = 0;
-    
-    
-    for(let i = 0; i < clickedEls.length; i++) {
+    clickedEls.forEach((item, index) => {
+        item.addEventListener('click', () => {
+            clickedEls.forEach((item, i) => {
+                if(index !== i){
+                    tab[i].classList.remove('activeSkill');
+                    item.classList.remove('activeSkill');
+                }
+            });
+            tab.forEach((item, i) => {
+                if(i === index){
+                    item.classList.toggle('activeSkill');
+                }
+            });
 
-
-        clickedEls[i].addEventListener('click', () => {
-            
-            if(clickedEls[i].classList.contains('activeSkill')){
-                activeEls = 0;
-
-                clickedEls[i].classList.remove('activeSkill');
-
-                gsap.fromTo(tab[i],{
-                    height: heigthOfTab
-                }, {
-                    duration: .4,
-                    height: 0
-                });
-
-            }
-            else{
-                new Promise(resolve => {
-                    ++activeEls;
-                    
-                    resolve()
-                }).then(() => {
-                    if(activeEls == 1) {
-                        clickedEls[i].classList.add('activeSkill');
-    
-                        gsap.fromTo(tab[i],{
-                            height: 0
-                        }, {
-                            duration: .4,
-                            height: heigthOfTab
-                        });
-                    }
-                });
-            }
+            item.classList.toggle('activeSkill');
         });
-    }
+
+    })
+    
+    
+    // for(let i = 0; i < clickedEls.length; i++) {
+
+
+    //     clickedEls[i].addEventListener('click', () => {
+            
+    //         if(clickedEls[i].classList.contains('activeSkill')){
+    //             activeEls = 0;
+
+    //             clickedEls[i].classList.remove('activeSkill');
+
+    //             gsap.fromTo(tab[i],{
+    //                 height: heigthOfTab
+    //             }, {
+    //                 duration: .4,
+    //                 height: 0
+    //             });
+
+    //         }
+    //         else{
+    //             new Promise(resolve => {
+    //                 ++activeEls;
+                    
+                //     resolve()
+                // }).then(() => {
+                //     if(activeEls == 1) {
+                //         clickedEls[i].classList.add('activeSkill');
+    
+                //         gsap.fromTo(tab[i],{
+    //                         height: 0
+    //                     }, {
+    //                         duration: .4,
+    //                         height: heigthOfTab
+    //                     });
+    //                 }
+    //             });
+    //         }
+    //     });
+    // }
   
 };
